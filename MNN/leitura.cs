207aa9b837a97eb9443b8b224d391fd40896f2ac
Italo Moraes <italo_kz@hotmail.com>
2015-09-03 23:57:00 -0300

    no message

---
+++ MNN/leitura.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MNN
+{
+    class leitura
+    {
+        public int MO
+        {
+            get; set;
+        }
+        public int EU
+        {
+            get; set;
+        }
+        public int EX
+        {
+            get; set;
+        }
+        public int CD
+        {
+            get; set;
+        }
+        public int C1
+        {
+            get; set;
+        }
+        public int C2
+        {
+            get; set;
+        }
+        public int C3
+        {
+            get; set;
+        }
+        public int D1
+        {
+            get; set;
+        }
+        public int D2
+        {
+            get; set;
+        }
+        public int DM
+        {
+            get; set;
+        }
+        public int R1
+        {
+            get; set;
+        }
+        public int R2
+        {
+            get; set;
+        }
+        public int R3
+        {
+            get; set;
+        }
+        public int R4
+        {
+            get; set;
+        }
+
+
+    }
+}
